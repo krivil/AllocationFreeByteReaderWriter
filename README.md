f50b8ee4c71306059eedac5fcf42724f444dff67
Kiril Markov <krivil@gmail.com>
2021-09-14 17:16:46 +0300

    Updated benchmark

--- README.md
+++ README.md
@@ -87,7 +87,7 @@ bool success = span.TryRead(out TestPersistable persistableValue, out Span<byte>
 
 ``` ini
 
-BenchmarkDotNet=v0.13.1, OS=ubuntu 20.04
+BenchmarkDotNet=v0.13.1, OS=Windows 10.0.22000
 Intel Core i9-9900K CPU 3.60GHz (Coffee Lake), 1 CPU, 16 logical and 8 physical cores
 .NET SDK=6.0.100-preview.7.21379.14
   [Host]     : .NET 6.0.0 (6.0.21.37719), X64 RyuJIT
@@ -95,27 +95,47 @@ Intel Core i9-9900K CPU 3.60GHz (Coffee Lake), 1 CPU, 16 logical and 8 physical 
 
 
 ```
-|                    Method |  Categories |        Mean |     Error |    StdDev |      Median |   Ratio | RatioSD | Completed Work Items | Lock Contentions |  Gen 0 | Allocated |
-|-------------------------- |------------ |------------:|----------:|----------:|------------:|--------:|--------:|---------------------:|-----------------:|-------:|----------:|
-|             SerializeByte |   Serialize |   0.4633 ns | 0.0047 ns | 0.0039 ns |   0.4642 ns |    1.00 |    0.00 |                    - |                - |      - |         - |
-|            SerializeInt32 |   Serialize |   0.4600 ns | 0.0044 ns | 0.0037 ns |   0.4600 ns |    0.99 |    0.01 |                    - |                - |      - |         - |
-|            SerializeInt64 |   Serialize |   0.4626 ns | 0.0045 ns | 0.0038 ns |   0.4620 ns |    1.00 |    0.01 |                    - |                - |      - |         - |
-|           SerializeSingle |   Serialize |   0.5379 ns | 0.0192 ns | 0.0180 ns |   0.5311 ns |    1.16 |    0.04 |                    - |                - |      - |         - |
-|           SerializeDouble |   Serialize |   0.4970 ns | 0.0029 ns | 0.0024 ns |   0.4964 ns |    1.07 |    0.01 |                    - |                - |      - |         - |
-|           SerializeString |   Serialize |  73.8650 ns | 0.5056 ns | 0.4730 ns |  73.9883 ns |  159.42 |    1.31 |                    - |                - | 0.0038 |      32 B |
-|         SerializeDateTime |   Serialize | 195.1607 ns | 1.0355 ns | 0.8084 ns | 195.0271 ns |  421.69 |    3.63 |                    - |                - |      - |         - |
-|   SerializeDateTimeOffset |   Serialize |  10.6000 ns | 0.2379 ns | 0.5655 ns |  10.3932 ns |   24.16 |    0.96 |                    - |                - |      - |         - |
-|      SerializePersistable |   Serialize |  73.8867 ns | 0.5216 ns | 0.4072 ns |  73.7765 ns |  159.66 |    1.94 |                    - |                - | 0.0038 |      32 B |
-|                           |             |             |           |           |             |         |         |                      |                  |        |           |
-|           DeserializeByte | Deserialize |   0.2916 ns | 0.0063 ns | 0.0053 ns |   0.2930 ns |   1.000 |    0.00 |                    - |                - |      - |         - |
-|          DeserializeInt32 | Deserialize |   0.2655 ns | 0.0083 ns | 0.0077 ns |   0.2654 ns |   0.911 |    0.03 |                    - |                - |      - |         - |
-|          DeserializeInt64 | Deserialize |   0.2761 ns | 0.0044 ns | 0.0039 ns |   0.2757 ns |   0.947 |    0.02 |                    - |                - |      - |         - |
-|         DeserializeSingle | Deserialize |   0.2562 ns | 0.0030 ns | 0.0025 ns |   0.2565 ns |   0.879 |    0.02 |                    - |                - |      - |         - |
-|         DeserializeDouble | Deserialize |   0.0000 ns | 0.0000 ns | 0.0000 ns |   0.0000 ns |   0.000 |    0.00 |                    - |                - |      - |         - |
-|         DeserializeString | Deserialize |   1.3641 ns | 0.0064 ns | 0.0056 ns |   1.3631 ns |   4.677 |    0.09 |                    - |                - |      - |         - |
-|       DeserializeDateTime | Deserialize |   0.2634 ns | 0.0112 ns | 0.0105 ns |   0.2621 ns |   0.895 |    0.03 |                    - |                - |      - |         - |
-| DeserializeDateTimeOffset | Deserialize |   1.2283 ns | 0.0118 ns | 0.0104 ns |   1.2286 ns |   4.219 |    0.09 |                    - |                - |      - |         - |
-|    DeserializePersistable | Deserialize |  45.0231 ns | 0.2592 ns | 0.2298 ns |  44.9424 ns | 154.425 |    3.32 |                    - |                - | 0.0076 |      64 B |
+|                        Method |                 Categories |        Mean |     Error |    StdDev |  Ratio | RatioSD |  Gen 0 | Allocated |
+|------------------------------ |--------------------------- |------------:|----------:|----------:|-------:|--------:|-------:|----------:|
+|                 SerializeByte |   Serialize,AllocationFree |   0.4047 ns | 0.0031 ns | 0.0026 ns |   1.00 |    0.00 |      - |         - |
+|                SerializeInt32 |   Serialize,AllocationFree |   0.4902 ns | 0.0039 ns | 0.0032 ns |   1.21 |    0.01 |      - |         - |
+|                SerializeInt64 |   Serialize,AllocationFree |   0.4609 ns | 0.0064 ns | 0.0054 ns |   1.14 |    0.02 |      - |         - |
+|               SerializeSingle |   Serialize,AllocationFree |   0.5561 ns | 0.0349 ns | 0.0358 ns |   1.38 |    0.10 |      - |         - |
+|               SerializeDouble |   Serialize,AllocationFree |   0.5876 ns | 0.0029 ns | 0.0024 ns |   1.45 |    0.01 |      - |         - |
+|               SerializeString |   Serialize,AllocationFree |  51.3988 ns | 0.1563 ns | 0.1462 ns | 127.08 |    0.92 | 0.0038 |      32 B |
+|             SerializeDateTime |   Serialize,AllocationFree |  82.5058 ns | 0.1614 ns | 0.1510 ns | 203.85 |    1.46 |      - |         - |
+|       SerializeDateTimeOffset |   Serialize,AllocationFree |   9.3971 ns | 0.0363 ns | 0.0340 ns |  23.23 |    0.20 |      - |         - |
+|          SerializePersistable |   Serialize,AllocationFree |  60.4435 ns | 0.1597 ns | 0.1333 ns | 149.37 |    1.14 | 0.0038 |      32 B |
+|                               |                            |             |           |           |        |         |        |           |
+|             SerializeByteJson |             Serialize,Json | 115.4455 ns | 0.9579 ns | 0.8960 ns |   1.00 |    0.00 | 0.0219 |     184 B |
+|            SerializeInt32Json |             Serialize,Json | 112.2672 ns | 0.2875 ns | 0.2549 ns |   0.97 |    0.01 | 0.0219 |     184 B |
+|            SerializeInt64Json |             Serialize,Json | 111.4606 ns | 0.4073 ns | 0.3810 ns |   0.97 |    0.01 | 0.0219 |     184 B |
+|           SerializeSingleJson |             Serialize,Json | 219.8054 ns | 1.1593 ns | 1.0844 ns |   1.90 |    0.02 | 0.0219 |     184 B |
+|           SerializeDoubleJson |             Serialize,Json | 218.3641 ns | 1.6525 ns | 1.3799 ns |   1.89 |    0.02 | 0.0219 |     184 B |
+|           SerializeStringJson |             Serialize,Json | 133.6537 ns | 0.9364 ns | 0.8301 ns |   1.16 |    0.01 | 0.0219 |     184 B |
+|         SerializeDateTimeJson |             Serialize,Json | 257.4679 ns | 1.3805 ns | 1.2913 ns |   2.23 |    0.02 | 0.0296 |     248 B |
+|   SerializeDateTimeOffsetJson |             Serialize,Json | 170.3939 ns | 1.3720 ns | 1.2833 ns |   1.48 |    0.02 | 0.0296 |     248 B |
+|      SerializePersistableJson |             Serialize,Json | 215.6746 ns | 0.8641 ns | 0.7660 ns |   1.87 |    0.02 | 0.0324 |     272 B |
+|                               |                            |             |           |           |        |         |        |           |
+|               DeserializeByte | Deserialize,AllocationFree |   0.2565 ns | 0.0050 ns | 0.0046 ns |   1.00 |    0.00 |      - |         - |
+|              DeserializeInt32 | Deserialize,AllocationFree |   0.4373 ns | 0.0056 ns | 0.0046 ns |   1.71 |    0.03 |      - |         - |
+|              DeserializeInt64 | Deserialize,AllocationFree |   0.4604 ns | 0.0044 ns | 0.0041 ns |   1.80 |    0.03 |      - |         - |
+|             DeserializeSingle | Deserialize,AllocationFree |   0.4365 ns | 0.0058 ns | 0.0051 ns |   1.71 |    0.04 |      - |         - |
+|             DeserializeDouble | Deserialize,AllocationFree |   0.4396 ns | 0.0043 ns | 0.0040 ns |   1.71 |    0.04 |      - |         - |
+|             DeserializeString | Deserialize,AllocationFree |   1.5146 ns | 0.0094 ns | 0.0088 ns |   5.91 |    0.11 |      - |         - |
+|           DeserializeDateTime | Deserialize,AllocationFree |   0.4697 ns | 0.0047 ns | 0.0044 ns |   1.83 |    0.04 |      - |         - |
+|     DeserializeDateTimeOffset | Deserialize,AllocationFree |   0.6007 ns | 0.0224 ns | 0.0209 ns |   2.34 |    0.10 |      - |         - |
+|        DeserializePersistable | Deserialize,AllocationFree |  26.3492 ns | 0.1198 ns | 0.1062 ns | 102.96 |    1.82 | 0.0076 |      64 B |
+|                               |                            |             |           |           |        |         |        |           |
+|           DeserializeByteJson |           Deserialize,Json | 110.9197 ns | 0.5239 ns | 0.4901 ns |   1.00 |    0.00 |      - |         - |
+|          DeserializeInt32Json |           Deserialize,Json | 113.4686 ns | 0.8414 ns | 0.7459 ns |   1.02 |    0.01 |      - |         - |
+|          DeserializeInt64Json |           Deserialize,Json | 113.2146 ns | 0.2604 ns | 0.2033 ns |   1.02 |    0.01 |      - |         - |
+|         DeserializeSingleJson |           Deserialize,Json | 158.1268 ns | 0.6575 ns | 0.6150 ns |   1.43 |    0.01 |      - |         - |
+|         DeserializeDoubleJson |           Deserialize,Json | 156.9926 ns | 0.2720 ns | 0.2124 ns |   1.42 |    0.01 |      - |         - |
+|         DeserializeStringJson |           Deserialize,Json | 137.1684 ns | 0.3681 ns | 0.3263 ns |   1.24 |    0.01 | 0.0038 |      32 B |
+|       DeserializeDateTimeJson |           Deserialize,Json | 140.7853 ns | 0.5063 ns | 0.4736 ns |   1.27 |    0.01 |      - |         - |
+| DeserializeDateTimeOffsetJson |           Deserialize,Json | 166.1167 ns | 0.5057 ns | 0.4223 ns |   1.50 |    0.01 |      - |         - |
+|    DeserializePersistableJson |           Deserialize,Json | 308.3187 ns | 1.1390 ns | 1.0097 ns |   2.78 |    0.02 | 0.0076 |      64 B |
 
 
 ## License
--- test/AllocationFreeByteReaderWriter.Benchmark/Benchmarks.cs
+++ test/AllocationFreeByteReaderWriter.Benchmark/Benchmarks.cs
@@ -3,9 +3,9 @@ using AllocationFreeByteReaderWriter;
 using BenchmarkDotNet.Configs;
 using AllocationFreeByteReaderWriter.Persistable;
 using System.Text;
+using System.Text.Json;
 
 [MemoryDiagnoser]
-[ThreadingDiagnoser]
 [CsvMeasurementsExporter]
 [RPlotExporter]
 [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
@@ -16,119 +16,192 @@ public class Benchmarks {
     private static readonly DateTimeOffset _dateOffset = DateTimeOffset.Now;
     private static readonly TestPersistable _persistable = new(){IntegerProperty = 123, StringProperty = "foo"};
 
-    private byte[] ByteBuffer = Array.Empty<byte>();
-    private byte[] Int32Buffer = Array.Empty<byte>();
-    private byte[] Int64Buffer = Array.Empty<byte>();
-    private byte[] SingleBuffer = Array.Empty<byte>();
-    private byte[] DoubleBuffer = Array.Empty<byte>();
-    private byte[] StringBuffer = Array.Empty<byte>();
-    private byte[] DateTimeBuffer = Array.Empty<byte>();
-    private byte[] DateTimeOffsetBuffer = Array.Empty<byte>();
-    private byte[] PersistableBuffer = Array.Empty<byte>();
+    private byte[] _byteBuffer = Array.Empty<byte>();
+    private byte[] _int32Buffer = Array.Empty<byte>();
+    private byte[] _int64Buffer = Array.Empty<byte>();
+    private byte[] _singleBuffer = Array.Empty<byte>();
+    private byte[] _doubleBuffer = Array.Empty<byte>();
+    private byte[] _stringBuffer = Array.Empty<byte>();
+    private byte[] _dateTimeBuffer = Array.Empty<byte>();
+    private byte[] _dateTimeOffsetBuffer = Array.Empty<byte>();
+    private byte[] _persistableBuffer = Array.Empty<byte>();
 
     [GlobalSetup]
     public void GlobalSetup() {
-        ByteBuffer = new byte[1];
-        ByteBuffer.AsSpan().TryWrite((byte)123, out _);
+        _byteBuffer = new byte[1];
+        _byteBuffer.AsSpan().TryWrite((byte)123, out _);
 
-        Int32Buffer = new byte[sizeof(int)];
-        Int32Buffer.AsSpan().TryWrite(123, out _);
+        _int32Buffer = new byte[sizeof(int)];
+        _int32Buffer.AsSpan().TryWrite(123, out _);
 
-        Int64Buffer = new byte[sizeof(long)];
-        Int64Buffer.AsSpan().TryWrite(123L, out _);
+        _int64Buffer = new byte[sizeof(long)];
+        _int64Buffer.AsSpan().TryWrite(123L, out _);
 
-        SingleBuffer = new byte[sizeof(float)];
-        SingleBuffer.AsSpan().TryWrite(123.0f, out _);
+        _singleBuffer = new byte[sizeof(float)];
+        _singleBuffer.AsSpan().TryWrite(123.0f, out _);
 
-        DoubleBuffer = new byte[sizeof(double)];
-        DoubleBuffer.AsSpan().TryWrite(123.0d, out _);
+        _doubleBuffer = new byte[sizeof(double)];
+        _doubleBuffer.AsSpan().TryWrite(123.0d, out _);
 
-        StringBuffer = new byte[sizeof(int) + System.Text.Encoding.UTF8.GetByteCount("foo")];
-        StringBuffer.AsSpan().TryWrite("foo", out _);
+        _stringBuffer = new byte[sizeof(int) + System.Text.Encoding.UTF8.GetByteCount("foo")];
+        _stringBuffer.AsSpan().TryWrite("foo", out _);
 
-        DateTimeBuffer = new byte[sizeof(long)];
-        DateTimeBuffer.AsSpan().TryWrite(_date, out _);
+        _dateTimeBuffer = new byte[sizeof(long)];
+        _dateTimeBuffer.AsSpan().TryWrite(_date, out _);
 
-        DateTimeOffsetBuffer = new byte[sizeof(long) + sizeof(short)];
-        DateTimeOffsetBuffer.AsSpan().TryWrite(_dateOffset, out _);
+        _dateTimeOffsetBuffer = new byte[sizeof(long) + sizeof(short)];
+        _dateTimeOffsetBuffer.AsSpan().TryWrite(_dateOffset, out _);
 
-        PersistableBuffer = new byte[_persistable.GetPersistedSizeInBytes()];
-        PersistableBuffer.AsSpan().TryWrite(_persistable, out _);
+        _persistableBuffer = new byte[_persistable.GetPersistedSizeInBytes()];
+        _persistableBuffer.AsSpan().TryWrite(_persistable, out _);
     }
 
-    [BenchmarkCategory("Serialize"), Benchmark(Baseline = true)]
+    [BenchmarkCategory("Serialize,AllocationFree"), Benchmark(Baseline = true)]
     public bool SerializeByte()
         => _buffer.AsSpan().TryWrite((byte)123, out _);
 
-    [BenchmarkCategory("Serialize"), Benchmark]
+    [BenchmarkCategory("Serialize,Json"), Benchmark(Baseline = true)]
+    public string SerializeByteJson()
+        => JsonSerializer.Serialize((byte)123);
+
+    [BenchmarkCategory("Serialize,AllocationFree"), Benchmark]
     public bool SerializeInt32()
         => _buffer.AsSpan().TryWrite(123, out _);
 
-    [BenchmarkCategory("Serialize"), Benchmark]
+    [BenchmarkCategory("Serialize,Json"), Benchmark]
+    public string SerializeInt32Json()
+        => JsonSerializer.Serialize(123);
+
+    [BenchmarkCategory("Serialize,AllocationFree"), Benchmark]
     public bool SerializeInt64()
         => _buffer.AsSpan().TryWrite(123L, out _);
 
-    [BenchmarkCategory("Serialize"), Benchmark]
+    [BenchmarkCategory("Serialize,Json"), Benchmark]
+    public string SerializeInt64Json()
+        => JsonSerializer.Serialize(123L);
+
+    [BenchmarkCategory("Serialize,AllocationFree"), Benchmark]
     public bool SerializeSingle()
         => _buffer.AsSpan().TryWrite(123.0f, out _);
 
-    [BenchmarkCategory("Serialize"), Benchmark]
+    [BenchmarkCategory("Serialize,Json"), Benchmark]
+    public string SerializeSingleJson()
+        => JsonSerializer.Serialize(123.0f);
+
+    [BenchmarkCategory("Serialize,AllocationFree"), Benchmark]
     public bool SerializeDouble()
         => _buffer.AsSpan().TryWrite(123.0d, out _);
 
-    [BenchmarkCategory("Serialize"), Benchmark]
+    [BenchmarkCategory("Serialize,Json"), Benchmark]
+    public string SerializeDoubleJson()
+        => JsonSerializer.Serialize(123.0d);
+
+    [BenchmarkCategory("Serialize,AllocationFree"), Benchmark]
     public bool SerializeString()
         => _buffer.AsSpan().TryWrite("foo", out _);
 
-    [BenchmarkCategory("Serialize"), Benchmark]
+    [BenchmarkCategory("Serialize,Json"), Benchmark]
+    public string SerializeStringJson()
+        => JsonSerializer.Serialize("foo");
+
+    [BenchmarkCategory("Serialize,AllocationFree"), Benchmark]
     public bool SerializeDateTime()
         => _buffer.AsSpan().TryWrite(_date, out _);
 
-    [BenchmarkCategory("Serialize"), Benchmark]
+    [BenchmarkCategory("Serialize,Json"), Benchmark]
+    public string SerializeDateTimeJson()
+        => JsonSerializer.Serialize(_date);
+
+    [BenchmarkCategory("Serialize,AllocationFree"), Benchmark]
     public bool SerializeDateTimeOffset()
         => _buffer.AsSpan().TryWrite(_dateOffset, out _);
 
-    [BenchmarkCategory("Serialize"), Benchmark] 
+    [BenchmarkCategory("Serialize,Json"), Benchmark]
+    public string SerializeDateTimeOffsetJson()
+        => JsonSerializer.Serialize(_dateOffset);
+
+    [BenchmarkCategory("Serialize,AllocationFree"), Benchmark]
     public bool SerializePersistable()
         => _buffer.AsSpan().TryWrite(_persistable, out _);
 
+    [BenchmarkCategory("Serialize,Json"), Benchmark]
+    public string SerializePersistableJson()
+        => JsonSerializer.Serialize(_persistable);
 
 
-    [BenchmarkCategory("Deserialize"), Benchmark(Baseline = true)]
+
+    [BenchmarkCategory("Deserialize,AllocationFree"), Benchmark(Baseline = true)]
     public bool DeserializeByte()
-        => new ReadOnlySpan<byte>(ByteBuffer).TryRead(out byte value, out _);
+        => new ReadOnlySpan<byte>(_byteBuffer).TryRead(out byte value, out _);
 
-    [BenchmarkCategory("Deserialize"), Benchmark]
+    [BenchmarkCategory("Deserialize,Json"), Benchmark(Baseline = true)]
+    public byte DeserializeByteJson()
+        => JsonSerializer.Deserialize<byte>("123");
+
+
+    [BenchmarkCategory("Deserialize,AllocationFree"), Benchmark]
     public bool DeserializeInt32()
-        => new ReadOnlySpan<byte>(ByteBuffer).TryRead(out int value, out _);
+        => new ReadOnlySpan<byte>(_byteBuffer).TryRead(out int value, out _);
 
-    [BenchmarkCategory("Deserialize"), Benchmark]
+    [BenchmarkCategory("Deserialize,Json"), Benchmark]
+    public int DeserializeInt32Json()
+        => JsonSerializer.Deserialize<int>("123");
+
+    [BenchmarkCategory("Deserialize,AllocationFree"), Benchmark]
     public bool DeserializeInt64()
-        => new ReadOnlySpan<byte>(ByteBuffer).TryRead(out long value, out _);
+        => new ReadOnlySpan<byte>(_byteBuffer).TryRead(out long value, out _);
 
-    [BenchmarkCategory("Deserialize"), Benchmark]
+    [BenchmarkCategory("Deserialize,Json"), Benchmark]
+    public long DeserializeInt64Json()
+        => JsonSerializer.Deserialize<long>("123");
+
+    [BenchmarkCategory("Deserialize,AllocationFree"), Benchmark]
     public bool DeserializeSingle()
-        => new ReadOnlySpan<byte>(ByteBuffer).TryRead(out float value, out _);
+        => new ReadOnlySpan<byte>(_byteBuffer).TryRead(out float value, out _);
 
-    [BenchmarkCategory("Deserialize"), Benchmark]
+    [BenchmarkCategory("Deserialize,Json"), Benchmark]
+    public float DeserializeSingleJson()
+        => JsonSerializer.Deserialize<float>("123.0");
+
+    [BenchmarkCategory("Deserialize,AllocationFree"), Benchmark]
     public bool DeserializeDouble()
-        => new ReadOnlySpan<byte>(ByteBuffer).TryRead(out double value, out _);
+        => new ReadOnlySpan<byte>(_byteBuffer).TryRead(out double value, out _);
 
-    [BenchmarkCategory("Deserialize"), Benchmark]
+    [BenchmarkCategory("Deserialize,Json"), Benchmark]
+    public double DeserializeDoubleJson()
+        => JsonSerializer.Deserialize<double>("123.0");
+
+    [BenchmarkCategory("Deserialize,AllocationFree"), Benchmark]
     public bool DeserializeString()
-        => new ReadOnlySpan<byte>(ByteBuffer).TryRead(out string value, out _);
+        => new ReadOnlySpan<byte>(_byteBuffer).TryRead(out string value, out _);
 
-    [BenchmarkCategory("Deserialize"), Benchmark]
+    [BenchmarkCategory("Deserialize,Json"), Benchmark]
+    public string? DeserializeStringJson()
+        => JsonSerializer.Deserialize<string>("\"foo\"");
+
+    [BenchmarkCategory("Deserialize,AllocationFree"), Benchmark]
     public bool DeserializeDateTime()
-        => new ReadOnlySpan<byte>(ByteBuffer).TryRead(out DateTime value, out _);
+        => new ReadOnlySpan<byte>(_byteBuffer).TryRead(out DateTime value, out _);
 
-    [BenchmarkCategory("Deserialize"), Benchmark]
+    [BenchmarkCategory("Deserialize,Json"), Benchmark]
+    public DateTime DeserializeDateTimeJson()
+        => JsonSerializer.Deserialize<DateTime>("\"2021-01-01T23:59:59\"");
+
+    [BenchmarkCategory("Deserialize,AllocationFree"), Benchmark]
     public bool DeserializeDateTimeOffset()
-        => new ReadOnlySpan<byte>(ByteBuffer).TryRead(out DateTimeOffset value, out _);
+        => new ReadOnlySpan<byte>(_byteBuffer).TryRead(out DateTimeOffset value, out _);
 
-    [BenchmarkCategory("Deserialize"), Benchmark]
+    [BenchmarkCategory("Deserialize,Json"), Benchmark]
+    public DateTimeOffset DeserializeDateTimeOffsetJson()
+        => JsonSerializer.Deserialize<DateTimeOffset>("\"2021-01-01T23:59:59+03:00\"");
+
+    [BenchmarkCategory("Deserialize,AllocationFree"), Benchmark]
     public bool DeserializePersistable()
-        => new ReadOnlySpan<byte>(PersistableBuffer).TryRead(out TestPersistable? value, out _);
+        => new ReadOnlySpan<byte>(_persistableBuffer).TryRead(out TestPersistable? value, out _);
+
+    [BenchmarkCategory("Deserialize,Json"), Benchmark]
+    public TestPersistable? DeserializePersistableJson()
+        => JsonSerializer.Deserialize<TestPersistable>("{\"IntegerProperty\": 123, \"StringProperty\": \"foo\"}");
 
 
     public record TestPersistable : IPersistable<TestPersistable> {
